fabe04e49e6cd5a10cc750cf405eaa1bc5b91ebd
4mohdisa <mohdisa233@gmail.com>
2025-12-06 21:18:49 +1030

    Initial Commit

---
+++ NeutralDrive/NeutralDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeutralDriveApp.swift
+//  NeutralDrive
+//
+//  Created by Mohammed Isa on 6/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NeutralDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
